9aa0f2057afa578554bfc41538b76318a20e0e08
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2025-08-13 05:42:52 +0000

    Initial commit

---
+++ src/pages/Home.c1dmp.js
@@ -0,0 +1,26 @@
+$w.onReady(function () {
+  $w("#subMenuBox").hide();
+  $w("#subMenuLiquidCooling").hide();
+
+  $w("#btnProducts").onMouseIn(() => {
+    $w("#subMenuBox").show("fade");
+  });
+
+  $w("#menuBox").onMouseOut(() => {
+    $w("#subMenuBox").hide("fade");
+    $w("#subMenuLiquidCooling").hide("fade");
+  });
+
+  $w("#btnLiquidCooling").onMouseIn(() => {
+    $w("#subMenuLiquidCooling").show("fade");
+  });
+
+  $w("#btnLiquidCooling").onMouseOut(() => {
+    $w("#subMenuLiquidCooling").hide("fade");
+  });
+});
+
+
+$w('#subMenuLiquidCooling').onMouseIn((#btnLiquidCooling) => {
+        
+})
